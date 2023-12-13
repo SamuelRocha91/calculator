998cb1a7fe1243594b4ada0331af8e1312d9c84e
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-12-13 11:29:50 -0300

    fim do mangue

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# calculator
+<h1>Project Drum Machine</h1>
+<p>Trata-se de um projeto desenvolvido no módulo de FrontEnd do curso de Desenvolvimento Web do FreeCodeCamp. O projeto envolveu o uso de Javascript, CSS e HTML através do framework React. Principais competências desenvolvidas: </p>
+<ol>
+  <li>React Hooks;</li>
+  <li>Lógica de programação;</li>
+  <li>Manipulação de estado;</li>
+  <li>Manipulação de bibliotecas;</li>
+</ol>
+
+<p>O projeto consiste num teclado de sons. Cada botão possui uma tonalidade própria e pode ser ativado mediante cliques do mouse ou no teclado. </p>
+<a  
+  href='https://drum-machine-opal-pi.vercel.app/'
+  target='_blank'
+  >
+  Ver aplicação
+</a>
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,61 @@
+import { useState } from 'react'
+import './App.css'
+
 
 function App() {
+  const [state, setState] = useState({
+    inputOperation: "0",
+    first: true
+  })
+  const handleClick = (event) => {
+    if (state.first) {
+      setState({ inputOperation: event.target.innerText, first: false})
+    } else {
+      setState((prevState) => ({
+        ...prevState,
+        inputOperation: `${prevState.inputOperation}${event.target.innerText}`
+      }))
+    }
+  }
 
+  const clearInput = () => {
+    setState({
+    inputOperation: "0",
+    first: true
+  })
+  }
   return (
-    <>
     <div>
-       <h1>Hello World</h1>
+      <div>
+        <p id="display"></p>
+        <input
+          value={state.inputOperation}
+          mame="inputOperation"
+          type="text"
+        />
+      </div>
+      <div onClick={handleClick}>
+        <button id="add">+</button>
+        <button id="subtract">-</button>
+        <button id="divide">/</button>
+        <button id="multiply">*</button>
+        <button id="decimal">.</button>
+        <button id="zero">0</button>
+        <button id="one">1</button>
+        <button id="two">2</button>
+        <button id="three">3</button>
+        <button id="four">4</button>
+        <button id="five">5</button>
+        <button id="six">6</button>
+        <button id="seven">7</button>
+        <button id="eight">8</button>
+        <button id="nine">9</button>
+      </div>
+      <div>
+        <button id="equals">=</button>
+        <button onClick={clearInput} id="clear">CE</button>
+      </div>
     </div>
-    </>
   )
 }
 
