63e5a470cd0d525c8a84f6645ffdcc33cb50ecc1
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-12-20 15:36:54 -0300

    feat: Implementa lógica de cálculos
    desenvolve a função handlecklick no arquivo app.jsx parsa maniipular cálculos

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,51 @@ import './App.css';
 function App() {
   const [state, setState] = useState({
     inputOperation: '0',
-    first: true,
+    result: '0',
   });
-  const handleClick = (event) => {
-    if (state.first) {
-      setState({ inputOperation: event.target.innerText, first: false });
+
+  const handleClick = (e) => {
+    const value = e.target.innerText;
+    const isDot = e.target.id === 'decimal';
+    let arrayOfInput = null;
+    arrayOfInput = state.inputOperation.split(/[-+*/]/);
+    const length = arrayOfInput.length - 1;
+    if (isDot && arrayOfInput[length].includes('.')) {
+    // eslint-disable-next-line no-else-return
+    } else if (arrayOfInput[length].length === 1 && arrayOfInput[length] === '0' && !isDot) {
+      const newInputOperation = `${state.inputOperation.slice(0, -1)}${value}`;
+      setState((prevState) => ({ ...prevState, inputOperation: newInputOperation }));
+    } else if (value.match(/[-+*/]/) != null) {
+      const arratTotal = state.inputOperation.split('');
+      const isOperator = arratTotal[arratTotal.length - 1].match(/[-+*/]/) != null;
+      const isOperator2 = arratTotal.length > 1 ? arratTotal[arratTotal.length - 2].match(/[+*/]/) != null : false;
+      if (isOperator2 && isOperator && value === '-') {
+        arratTotal.pop();
+        arratTotal.push(value);
+      } else if (isOperator2 && isOperator) {
+        arratTotal.pop();
+        arratTotal.pop();
+        arratTotal.push(value);
+      } else if (isOperator && value !== '-') {
+        arratTotal.pop();
+        arratTotal.push(value);
+      } else {
+        arratTotal.push(value);
+      }
+
+      setState((prevState) => ({ ...prevState, inputOperation: `${arratTotal.join('')}` }));
+    } else if (value === '=') {
+      // eslint-disable-next-line no-eval, padded-blocks
+      setState((prevState) => ({ ...prevState, inputOperation: `${eval(prevState.inputOperation)}` }));
     } else {
-      setState((prevState) => ({
-        ...prevState,
-        inputOperation: `${prevState.inputOperation}${event.target.innerText}`,
-      }));
+      setState((prevState) => ({ ...prevState, inputOperation: `${prevState.inputOperation}${value}` }));
     }
   };
 
   const clearInput = () => {
     setState({
       inputOperation: '0',
+      result: '0',
       first: true,
     });
   };
@@ -28,36 +57,37 @@ function App() {
       <div id='calculator'>
         <div>
           <section className='displayArea'>
-            <p id="display"></p>
+            <p id="display">{ state.inputOperation }</p>
           </section>
         </div>
-        <div onClick={handleClick}>
+        <div>
           <input
             value={state.inputOperation}
             name="inputOperation"
             type="text"
           />
         </div>
-        <section>
-            <div>
-              <button id="add">9</button>
-              <button id="subtract">8</button>
-              <button id="divide">7</button>
-              <button id="multiply">6</button>
-              <button id="decimal">5</button>
-              <button id="zero">4</button>
-              <button id="one">3</button>
+        <section >
+            <div onClick={handleClick}>
+              <button id="nine">9</button>
+              <button id="eight">8</button>
+              <button id="seven">7</button>
+              <button id="six">6</button>
+              <button id="five">5</button>
+              <button id="four">4</button>
+              <button id="three">3</button>
               <button id="two">2</button>
-              <button id="three">1</button>
-              <button id="four">0</button>
-              <button id="five">.</button>
+              <button id="one">1</button>
+              <button id="zero">0</button>
+              <button id="decimal">.</button>
               <button id="equals">=</button>
             </div>
             <div>
               <button onClick={clearInput} id="clear">CE</button>
-              <button id="six">*</button>
-              <button>-</button>
-              <button>/</button>
+              <button onClick={handleClick} id="multiply">*</button>
+              <button onClick={handleClick} id='subtract'>-</button>
+              <button onClick={handleClick} id="divide">/</button>
+              <button onClick={handleClick} id="add">+</button>
             </div>
         </section>
       </div>
